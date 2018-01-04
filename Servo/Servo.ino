2afa57dca91c2c01ebe27d183573858360862f71
Chia-Chun Wu <ccwu0918@gmail.com>
2018-01-05 01:03:24 +0800

    Add files via upload

---
+++ Servo/Servo.ino
@@ -0,0 +1,29 @@
+int val = 0; 
+int controlPin = 2; 
+int buttonPin = 6; 
+ 
+void pin_change(void) 
+{ 
+  val = !val; 
+} 
+ 
+void setup() { 
+  Serial.begin(9600); 
+  pinMode(controlPin, OUTPUT); 
+  attachInterrupt(buttonPin, pin_change, RISING); 
+} 
+ 
+void loop() { 
+  if (val) 
+  { 
+      digitalWrite(controlPin, HIGH); 
+      Serial.println("High"); 
+  } 
+  else 
+  { 
+      digitalWrite(controlPin, LOW); 
+      Serial.println("Low"); 
+  } 
+ 
+  delay(500); 
+}
